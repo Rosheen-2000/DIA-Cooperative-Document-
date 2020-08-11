2cea94e7b69a5522256ce3d6ff3f5e8643d45f73
St2r <St2r@github.com>
2020-08-11 17:19:53 +0800

    写了一个login接口

---
+++ frontend/src/app/routes/passport/passport.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PassportService {
+  private readonly baseUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.baseUrl = environment.baseUrl;
+  }
+
+  public login(uname: string, pwd: string): Observable<any> {
+    return this.http.post<{
+      msg: string, token: string
+    }>(this.baseUrl + '/api/passport/login', {
+      uname,
+      pwd
+    });
+  }
+
+  public logout(): void {
+
+  }
+
+  public register(): void {
+
+  }
+}
